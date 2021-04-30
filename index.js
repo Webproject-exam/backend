86b701ca36d87ca2af9c586da8cd091400d334de
Cornelius Sandmal <sandmal.cornelius@gmail.com>
2021-04-30 13:32:48 +0200

    Modify: changed dashboard route to manage

--- index.js
+++ index.js
@@ -1,52 +1,52 @@
-require('dotenv').config();
-require('./auth/user.auth');
+require("dotenv").config();
+require("./auth/user.auth");
 
-const express = require('express');
-const mongoose = require('mongoose');
-const passport = require('passport');
-const cors = require('cors');
-const morgan = require('morgan');
-const cookieParser = require('cookie-parser');
-const swaggerUi = require('swagger-ui-express');
-const swaggerDocument = require('./swagger.json');
+const express = require("express");
+const mongoose = require("mongoose");
+const passport = require("passport");
+const cors = require("cors");
+const morgan = require("morgan");
+const cookieParser = require("cookie-parser");
+const swaggerUi = require("swagger-ui-express");
+const swaggerDocument = require("./swagger.json");
 
 const app = express();
 
 // Route handlers
-const dashboardRoute = require('./routes/dashboard.routes');
-const profileRoute = require('./routes/profile.routes');
-const resetRoute = require('./routes/email.routes');
-const authRoute = require('./routes/auth.routes');
+const dashboardRoute = require("./routes/manage.routes");
+const profileRoute = require("./routes/profile.routes");
+const resetRoute = require("./routes/email.routes");
+const authRoute = require("./routes/auth.routes");
 
 // Middlewares
 // parse request of content-type - application/json
 app.use(express.json());
 // parse request of content-type - application/x-www-form-urlencoded
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 // read cookie information
 app.use(cookieParser());
 // Not whitelisted atm, this is for development purposes
 if (
   process.env &&
   process.env.NODE_ENV &&
-  process.env.NODE_ENV === 'production'
+  process.env.NODE_ENV === "production"
 ) {
-  app.use(cors({credentials: true, origin: process.env.FRONTENDHOST}));
+  app.use(cors({ credentials: true, origin: process.env.FRONTENDHOST }));
 } else {
   app.use(cors());
 }
 // Easier to see what requests are sent via postman
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 // Authenticate user
-const authUser = passport.authenticate('jwt', {session: false});
-const hasRole = require('./middleware/role.middleware');
+const authUser = passport.authenticate("jwt", { session: false });
+const hasRole = require("./middleware/role.middleware");
 // Add swagger
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
-app.use('/', authRoute);
-app.use('/dashboard', authUser, hasRole.Manager, dashboardRoute);
-app.use('/profile', authUser, hasRole.User, profileRoute);
-app.use('/reset_password', resetRoute);
+app.use("/", authRoute);
+app.use("/manage", authUser, hasRole.Manager, dashboardRoute);
+app.use("/profile", authUser, hasRole.User, profileRoute);
+app.use("/reset_password", resetRoute);
 
 // Connect to DB
 mongoose
@@ -56,8 +56,8 @@ mongoose
     useCreateIndex: true,
     useFindAndModify: false,
   })
-  .then(() => console.log('Connected to DB!'))
-  .catch(error => console.log(error));
+  .then(() => console.log("Connected to DB!"))
+  .catch((error) => console.log(error));
 
 // Start server
 const port = process.env.PORT;
@@ -67,5 +67,5 @@ app.listen(port, () => {
 
 // Handle errors.
 app.use((error, req, res, next) => {
-  res.status(error.status || 500).json({error});
+  res.status(error.status || 500).json({ error });
 });
