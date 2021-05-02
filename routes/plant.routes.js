bfb7c3e7ac0f9f0e6f7fbcf1f72d2d54d20587c9
Cornelius Sandmal <sandmal.cornelius@gmail.com>
2021-05-02 22:03:18 +0200

    Added jwt-decode to use for plant controller

--- routes/plant.routes.js
+++ routes/plant.routes.js
@@ -1,8 +1,13 @@
 const express = require("express");
-const { getAllPlants, getPlant } = require("../controllers/plant.controller");
+const {
+  getAllPlants,
+  getPlant,
+  updatePlant,
+} = require("../controllers/plant.controller");
 
 const router = express.Router();
 
 router.get("/", getAllPlants);
 router.get("/:id", getPlant);
+router.patch("/:id", updatePlant);
 module.exports = router;
