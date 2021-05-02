a7e0b7c4ce3feab1f22ee0e2793ef5a4f244902e
Cornelius Sandmal <sandmal.cornelius@gmail.com>
2021-05-02 20:06:59 +0200

    Changed so there is manage/users && manage/plants. Added plant methods

--- routes/manage.routes.js
+++ routes/manage.routes.js
@@ -4,33 +4,72 @@ const {
   updateUser,
   deleteUser,
   getAllUsers,
+  createPlant,
+  deletePlant,
 } = require("../controllers/manage.controller");
 
 const router = express.Router();
+
+//
+// @USER
+//
+
 /**
  * POST
  * req.body = name, surname, role, email, password
  */
-router.post("/", createUser);
+router.post("/users", createUser);
 
 /**
  * GET
  * Get all users
  * Returns name, surname, email, role
  */
-router.get("/", getAllUsers);
+router.get("/users", getAllUsers);
 
 /**
  * PATCH
  * Update users profile
  * req.body = name, surname, email, role
  */
-router.patch("/", updateUser);
+router.patch("/users", updateUser);
 
 /**
  * DELETE
  * req.body = email
  */
-router.delete("/", deleteUser);
+router.delete("/users", deleteUser);
+
+//
+// @Plant
+//
+
+/**
+ * POST
+ * Create new plant
+ *	req.body =
+ *						name,
+ *						building,
+ *						floor,
+ *						room,
+ *						waterFrequency,
+ *						waterNext,
+ *						responsible,
+ *						fertFrequency,
+ *						fertNext,
+ *						lighting,
+ *						description,
+ *						placement,
+ *						water,
+ *						nutrition,
+ */
+router.post("/plants", createPlant);
+
+/**
+ * POST
+ * Create new plant
+ *	req.params = id
+ */
+router.delete("/plants/:id", deletePlant);
 
 module.exports = router;
