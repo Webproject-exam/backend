df888c5806640bcc3c781c31e846dc9fe93d41e8
Sandmal <sandmal.cornelius@gmail.com>
2021-05-07 20:05:55 +0200

    Edit prettier.rc, fixed watering route and updatePlantCare is in progress

--- controllers/plant.controller.js
+++ controllers/plant.controller.js
@@ -3,82 +3,174 @@ const User = require('../models/User');
 const jwtDecode = require('jwt-decode');
 
 exports.getAllPlants = async (req, res) => {
-  try {
-    const allPlants = await Plant.find({}, [
-      'name',
-      'placement',
-      'watering.waterNext',
-      'watering.waterFrequency',
-      'lighting',
-      'fertilization.fertAmount',
-      'fertilization.fertFrequency',
-      'fertilization.fertNext',
-    ]);
-    res.status(200).json(allPlants);
-  } catch (error) {
-    res.status(500).send({
-      message: 'Internal server error when fetching all plants',
-      error,
-    });
-  }
+   try {
+      const allPlants = await Plant.find({}, [
+         'name',
+         'placement',
+         'watering.waterNext',
+         'watering.waterFrequency',
+         'lighting',
+         'fertilization.fertAmount',
+         'fertilization.fertFrequency',
+         'fertilization.fertNext',
+      ]);
+      res.status(200).json(allPlants);
+   } catch (error) {
+      res.status(500).send({
+         message: 'Internal server error when fetching all plants',
+         error,
+      });
+   }
 };
 
 exports.getPlant = async (req, res) => {
-  try {
-    const plant = await Plant.findOne({_id: req.params.id}, [
-      'name',
-      'lighting',
-      'responsible',
-      'placement',
-      'watering',
-      'fertilization',
-      'information',
-      'createdAt',
-    ]);
-    res.status(200).json({plant});
-  } catch (error) {
-    res
-      .status(500)
-      .send({message: 'Internal server error when fetching plant', error});
-  }
+   try {
+      const plant = await Plant.findOne(
+         {
+            _id: req.params.id,
+         },
+         [
+            'name',
+            'lighting',
+            'responsible',
+            'placement',
+            'watering',
+            'fertilization',
+            'information',
+            'createdAt',
+         ]
+      );
+      res.status(200).json({
+         plant,
+      });
+   } catch (error) {
+      res.status(500).send({
+         message: 'Internal server error when fetching plant',
+         error,
+      });
+   }
 };
 
 exports.waterPlant = async (req, res) => {
-  const {id, waterNext} = req.body;
-  console.log(req.body);
-  // Checks if there is any headers in request
-  const headers = req.headers.authorization;
-  if (!headers) return res.status(401).send({error: 'Unauthorized'});
+   const { selectedPlant, waterNext, fertNext } = req.body;
+   // Checks if there is any headers in request
+   const headers = req.headers.authorization;
+   if (!headers)
+      return res.status(401).send({
+         error: 'Unauthorized',
+      });
 
-  const token = headers.split(' ')[1];
-  const {_id, role, exp} = jwtDecode(token);
+   const token = headers.split(' ')[1];
+   const { _id, role, exp } = jwtDecode(token);
 
-  //Verify token
-  if (Date.now() >= exp * 1000)
-    return res.status(401).send({error: 'Unauthorized'});
+   //Verify token
+   if (Date.now() >= exp * 1000)
+      return res.status(401).send({
+         error: 'Unauthorized',
+      });
 
-  if (!role == 'manager' || !role === 'gardener')
-    return res.status(401).send({error: 'Unauthorized'});
+   if (!role == 'manager' || !role === 'gardener')
+      return res.status(401).send({
+         error: 'Unauthorized',
+      });
 
-  //finds user in DB to find name
-  const user = await User.findOne({_id});
-  const updatedPlant = {
-    watering: {
-      waterNext,
-      lastWateredBy: user.name,
-      lastWateredDate: Date.now(),
-    },
-  };
-  // console.log(updatedPlant);
-  try {
-    // decode token to get role and user id
-    // Update plant based on id
-    const plant = await Plant.updateOne({_id: id}, {$set: updatedPlant});
-    // console.log(plant);
-    return res.status(200).json({message: plant});
-  } catch (error) {
-    res
-      .status(500)
-      .send({error: 'Internal server error when updating plant', error});
-  }
+   //finds user in DB to find name
+   const user = await User.findOne({
+      _id,
+   });
+   let updatedPlant;
+   if (waterNext) {
+      updatedPlant = {
+         'watering.waterNext': waterNext,
+         'watering.lastWateredBy': user.name,
+         'watering.lastWateredDate': Date.now(),
+      };
+   }
+   if (fertNext) {
+      updatedPlant = {
+         'fertilization.fertNext': fertNext,
+         'fertilization.lastFertBy': user.name,
+         'fertilization.lastFertDate': Date.now(),
+      };
+   }
+   try {
+      // decode token to get role and user id
+      // Update plant based on id
+      await Plant.updateOne(
+         {
+            _id: selectedPlant,
+         },
+         {
+            $set: updatedPlant,
+         }
+      );
+      return res.status(200).json({
+         message: 'Plant updated',
+      });
+   } catch (error) {
+      res.status(500).send({
+         error: 'Internal server error when updating plant',
+         error,
+      });
+   }
+};
+
+exports.updatePlantCare = async (req, res) => {
+   const { id } = req.params;
+   const { waterNext, fertNext, lastPostponedReason } = req.body;
+   // Checks if there is any headers in request
+   const headers = req.headers.authorization;
+   if (!headers)
+      return res.status(401).send({
+         error: 'Unauthorized',
+      });
+
+   const token = headers.split(' ')[1];
+   const { _id, role, exp } = jwtDecode(token);
+
+   //Verify token
+   if (Date.now() >= exp * 1000)
+      return res.status(401).send({
+         error: 'Unauthorized',
+      });
+
+   if (!role == 'manager' || !role === 'gardener')
+      return res.status(401).send({
+         error: 'Unauthorized',
+      });
+
+   //finds user in DB to find name
+   const user = await User.findOne({
+      _id,
+   });
+   let updatedPlant;
+   if (waterNext) {
+      updatedPlant = {
+         'watering.waterNext': waterNext,
+         'watering.lastWateredBy': user.name,
+         'watering.lastWateredDate': Date.now(),
+      };
+      if (lastPostponedReason)
+         return { 'watering.lastPostponedReason': lastPostponedReason };
+   }
+   try {
+      // decode token to get role and user id
+      // Update plant based on id
+      await Plant.updateOne(
+         {
+            _id: id,
+         },
+         {
+            $set: updatedPlant,
+         }
+      );
+      return res.status(200).json({
+         message: 'Plant updated',
+      });
+   } catch (error) {
+      res.status(500).send({
+         error: 'Internal server error when updating plant',
+         error,
+      });
+   }
 };
--- models/Plant.js
+++ models/Plant.js
@@ -1,135 +1,137 @@
 const mongoose = require('mongoose');
 
 const plantSchema = new mongoose.Schema(
-  {
-    name: {
-      type: String,
-      required: true,
-      trim: true,
-    },
-    responsible: {
-      type: String,
-      trim: true,
-    },
-    lighting: {
-      type: String,
-      trim: true,
-      required: true,
-      enum: [
-        'sunlight',
-        'sunlight / half shade',
-        'half shade',
-        'half shade / shade',
-        'shade',
-      ],
-    },
-    placement: {
-      building: {
-        type: String,
-        required: true,
-        trim: true,
+   {
+      name: {
+         type: String,
+         required: true,
+         trim: true,
       },
-      floor: {
-        type: Number,
-        max: 4,
-        min: 0,
-        required: true,
-        trim: true,
+      responsible: {
+         type: String,
+         trim: true,
       },
-      room: {
-        type: String,
-        required: true,
-        trim: true,
-      },
-    },
-    watering: {
-      waterFrequency: {
-        type: Number,
-        required: true,
-        trim: true,
-      },
-      waterNext: {
-        type: Date,
-        trim: true,
-      },
-      waterAmount: {
-        type: String,
-        required: true,
-        trim: true,
-        enum: ['plentiful', 'average', 'sparse'],
-      },
-      lastWateredBy: {
-        type: String,
-        trim: true,
-      },
-      lastWateredDate: {
-        type: Number,
-        trim: true,
-      },
-      lastPostponedReason: {
-        type: String,
-        trim: true,
-      },
-    },
-    fertilization: {
-      fertFrequency: {
-        type: Number,
-        required: true,
-        trim: true,
-      },
-      fertNext: {
-        type: Date,
-        trim: true,
-      },
-      fertAmount: {
-        type: String,
-        required: true,
-        trim: true,
-        enum: ['plentiful', 'average', 'sparse'],
-      },
-      lastFertBy: {
-        type: String,
-        trim: true,
-      },
-      lastFertDate: {
-        type: Number,
-        trim: true,
-      },
-      lastPostponedReason: {
-        type: String,
-        trim: true,
-      },
-    },
-    information: {
-      description: {
-        type: String,
-        trim: true,
+      lighting: {
+         type: String,
+         trim: true,
+         required: true,
+         enum: [
+            'sunlight',
+            'sunlight / half shade',
+            'half shade',
+            'half shade / shade',
+            'shade',
+         ],
       },
       placement: {
-        type: String,
-        trim: true,
+         building: {
+            type: String,
+            required: true,
+            trim: true,
+         },
+         floor: {
+            type: Number,
+            max: 4,
+            min: 0,
+            required: true,
+            trim: true,
+         },
+         room: {
+            type: String,
+            required: true,
+            trim: true,
+         },
       },
       watering: {
-        type: String,
-        trim: true,
+         waterFrequency: {
+            type: Number,
+            required: true,
+            trim: true,
+         },
+         waterNext: {
+            type: Date,
+            trim: true,
+         },
+         waterAmount: {
+            type: String,
+            required: true,
+            trim: true,
+            enum: ['plentiful', 'average', 'sparse'],
+         },
+         lastWateredBy: {
+            type: String,
+            trim: true,
+         },
+         lastWateredDate: {
+            type: Number,
+            trim: true,
+         },
+         lastPostponedReason: {
+            type: String,
+            trim: true,
+            default: 'No reason given',
+         },
       },
-      nutrition: {
-        type: String,
-        trim: true,
+      fertilization: {
+         fertFrequency: {
+            type: Number,
+            required: true,
+            trim: true,
+         },
+         fertNext: {
+            type: Date,
+            trim: true,
+         },
+         fertAmount: {
+            type: String,
+            required: true,
+            trim: true,
+            enum: ['plentiful', 'average', 'sparse'],
+         },
+         lastFertBy: {
+            type: String,
+            trim: true,
+         },
+         lastFertDate: {
+            type: Number,
+            trim: true,
+         },
+         lastPostponedReason: {
+            type: String,
+            trim: true,
+            default: 'No reason given',
+         },
       },
-    },
-  },
-  {timestamps: true}
+      information: {
+         description: {
+            type: String,
+            trim: true,
+         },
+         placement: {
+            type: String,
+            trim: true,
+         },
+         watering: {
+            type: String,
+            trim: true,
+         },
+         nutrition: {
+            type: String,
+            trim: true,
+         },
+      },
+   },
+   { timestamps: true }
 );
 
 plantSchema.set('toJson', {
-  virtuals: true,
-  versionKey: false,
-  transform(doc, ret) {
-    // remove these props when object is serialized
-    delete ret._id;
-    delete ret.__v;
-  },
+   virtuals: true,
+   versionKey: false,
+   transform(doc, ret) {
+      // remove these props when object is serialized
+      delete ret._id;
+      delete ret.__v;
+   },
 });
 
 module.exports = mongoose.model('Plant', plantSchema);
